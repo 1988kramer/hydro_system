e33014fdf0d029005ac50586f13f7f988aea82f7
Andrew Kramer <1988kramer@gmail.com>
2021-01-15 21:03:48 -0700

    changed to 2 motor

--- nodes/motor_hat_tester.py
+++ nodes/motor_hat_tester.py
@@ -9,34 +9,23 @@ from hydro_system.msg import MotorHatCmd
 
 
 def command_publisher():
-  global speed
-  global direction
-  global increase
+  global motor
   global command_pub
 
-  if speed == 0:
-    rospy.loginfo('increasing speed')
-    increase = True
-    if direction == 'forward':
-      direction = 'backward'
-    else:
-      direction = 'forward'
-  elif speed == 255:
-    rospy.loginfo('decreasing speed')
-    increase = False
-
-  if increase:
-    speed += 1
-  else: 
-    speed -= 1
+  if motor == 0:
+    motor = 1
+  else:
+    motor = 0
 
   msg = MotorHatCmd()
-  msg.commands = [direction, 'release', 'release', 'release']
-  msg.speeds = [speed, 0, 0, 0]
+  msg.commands = ['release'] * 4
+  msg.commands[motor] = 'forward'
+  msg.speeds = [0] * 4
+  msg.speeds[motor] = 128
 
   command_pub.publish(msg)
 
-  rospy.sleep(0.05)
+  rospy.sleep(2.0)
 
 
 
@@ -45,9 +34,7 @@ if __name__ == '__main__':
   rospy.init_node('motor_tester', anonymous=True)
   command_pub = rospy.Publisher('/motor_hat_cmd', MotorHatCmd, queue_size=1)
 
-  direction = 'forward'
-  increase = True
-  speed = 0
+  motor = 1
 
   while not rospy.is_shutdown():
     try:
