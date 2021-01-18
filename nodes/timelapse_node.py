e20821695f68799ab22a555084c3ddef21cd8dad
Andrew Kramer <1988kramer@gmail.com>
2021-01-17 20:13:45 -0700

    got push to google drive working, fixed timing issues

--- nodes/timelapse_node.py
+++ nodes/timelapse_node.py
@@ -12,25 +12,27 @@ import os
 class Timelapse:
   def __init__(self):
     rospy.init_node('timelapse', anonymous=True)
-    self.camera = cv2.VideoCapture(0)
+    #self.camera = cv2.VideoCapture(0)
     self.interval = 600 # default 10 min interval
     self.intensity_thresh = 32
 
   def take_image(self):
     start_time = rospy.Time.now()
-    _, image = self.camera.read()
+    camera = cv2.VideoCapture(0)
+    _, image = camera.read()
+    stamp = rospy.Time.now().to_sec()
+    del(camera)
 
     # check if average intensity is above threshold
     gray_im = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     mean = cv2.mean(gray_im)[0]
     rospy.loginfo('mean image intensity: %d' % mean)
 
-    stamp = rospy.Time.now().to_sec()
-
     if mean >= self.intensity_thresh:
       rospy.loginfo('saving image')
       filename = '/home/pi/logs/images/%d.jpg' % int(stamp)
-      cv2.imwrite(filename, image) 
+      cv2.imwrite(filename, image)
+      os.system('rclone copy ' + filename + ' remote_logs:personal\ projects/hydroponics/logs/images/')
 
     duration = rospy.Time.now() - start_time
     rospy.sleep(self.interval - duration.to_sec())
