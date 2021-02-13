26e02058754fab658e46a7a14b72707206115b5c
Andrew Kramer <1988kramer@gmail.com>
2021-02-13 13:25:37 -0700

    added chi square confidence-based outlier rejection

--- nodes/filter_node.py
+++ nodes/filter_node.py
@@ -18,6 +18,7 @@ class FilterNode:
     self.q = rospy.get_param('~q')
     self.r = rospy.get_param('~r')
     self.var = rospy.get_param('~var')
+    self.chi2 = rospy.get_param('~chi2', 0.0)
     self.last_t = 0.0
 
 
@@ -30,15 +31,18 @@ class FilterNode:
       self.last_t = stamp
     else:
 
-      dt = stamp - self.last_t
-      self.last_t = stamp
       y_tilde = msg.value - self.x
-      var_hat = self.var + self.q * dt
-      S = var_hat + self.r
-      K = var_hat / S
-      x_hat = self.x + K * y_tilde
-      self.var = (1.0 - K) * var_hat
-      self.x = x_hat
+
+      # reject outliers given a chi^2 confidence value
+      if self.chi2 > 0.0 and (y_tilde**2 / self.var) <= self.chi2:
+        dt = stamp - self.last_t
+        self.last_t = stamp
+        var_hat = self.var + self.q * dt
+        S = var_hat + self.r
+        K = var_hat / S
+        x_hat = self.x + K * y_tilde
+        self.var = (1.0 - K) * var_hat
+        self.x = x_hat
 
     out_msg = StampedFloatWithVariance()
     out_msg.header = msg.header
