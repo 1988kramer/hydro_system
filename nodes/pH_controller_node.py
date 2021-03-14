aa9350ff8c26d7a176676f623535e8b48015861d
Andrew Kramer <1988kramer@gmail.com>
2021-03-14 13:43:24 -0600

    increased adjust duration for up

--- nodes/pH_controller_node.py
+++ nodes/pH_controller_node.py
@@ -90,7 +90,7 @@ class pH_ControllerNode:
     self.motor_cmd_pub.publish(self.start_msg)
 
     if motor == self.up_motor:
-      rospy.sleep(15.0)
+      rospy.sleep(16.0)
     else:
       rospy.sleep(10.0)
 
