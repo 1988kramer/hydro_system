d52ba6010a1d493a6749d839316ec812d630580c
Andrew Kramer <1988kramer@gmail.com>
2022-05-03 19:50:07 -0600

    went back to plotting a single day

--- plotly/app.py
+++ plotly/app.py
@@ -28,36 +28,27 @@ app.layout = html.Div(
 def get_data(name):
     today_str = datetime.today().strftime('_%d_%m_%Y')
     yesterday_str = (datetime.today() - timedelta(1)).strftime('_%d_%m_%Y')
-    day_before_str = (datetime.today() - timedelta(2)).strftime('_%d_%m_%Y')
     directory = '/home/pi/logs/'
     data_today_fname = directory + name + today_str + '.csv'
     data_yesterday_fname = directory + name + yesterday_str + '.csv'
-    data_day_before_fname = directory + name + day_before_str + '.csv'
     data_filt_today_fname = directory + name + '_filtered' + today_str + '.csv'
     data_filt_yesterday_fname = directory + name + '_filtered' + yesterday_str + '.csv'
-    data_filt_day_before_fname = directory + name + '_filtered' + day_before_str + '.csv'
 
     data = np.loadtxt(data_today_fname, delimiter=',')
     if os.path.isfile(data_yesterday_fname):
         data_yesterday = np.loadtxt(data_yesterday_fname, delimiter=',')
         data = np.concatenate((data_yesterday,data))
-    if os.path.isfile(data_day_before_fname):
-        data_day_before = np.loadtxt(data_day_before_fname, delimiter=',')
-        data = np.concatenate((data_day_before, data))
 
     data_filt = np.loadtxt(data_filt_today_fname, delimiter=',')
     if os.path.isfile(data_filt_yesterday_fname):
         data_filt_yesterday = np.loadtxt(data_filt_yesterday_fname, delimiter=',')
         data_filtered = np.concatenate((data_filt_yesterday, data_filt))
-    if os.path.isfile(data_filt_day_before_fname):
-        data_filt_day_before = np.loadtxt(data_filt_day_before_fname, delimiter=',')
-        data_filtered = np.concatenate((data_filt_day_before, data_filt))
 
-    data = data[data[-1,0] - data[:,0] < 172800.0]
+    data = data[data[-1,0] - data[:,0] < 84600.0]
     data[:,0] -= data[0,0]
     data[:,0] -= data[-1,0]
     data[:,0] /= 3600.
-    data_filtered = data_filtered[data_filtered[-1,0] - data_filtered[:,0] < 172800.0]
+    data_filtered = data_filtered[data_filtered[-1,0] - data_filtered[:,0] < 84600.0]
     data_filtered[:,0] -= data_filtered[0,0]
     data_filtered[:,0] -= data_filtered[-1,0]
     data_filtered[:,0] /= 3600.
@@ -85,7 +76,7 @@ def update_plots_live(n):
     air_temp_title += deg_sign + 'C)'
     humidity_title = 'Relative Humidity (current: %.2f' % humidity_filtered[-1,1]
     humidity_title +=  '%)'
-    depth_title = 'Water Level (current: %.1f L)' % water_depth[-1,1]
+    depth_title = 'Water Volume (current: %.1f L)' % water_depth[-1,1]
 
     # Create the graph with subplots
     fig = plotly.subplots.make_subplots(rows=5, cols=1, vertical_spacing=0.1,
@@ -172,7 +163,7 @@ def update_plots_live(n):
     fig.append_trace({
         'x': water_depth[:,0],
         'y': water_depth[:,1],
-        'name': 'water depth raw',
+        'name': 'water volume raw',
         'mode': 'lines',
         'type': 'scatter',
         'line': {'dash': 'dash', 'color': 'red'},
@@ -181,7 +172,7 @@ def update_plots_live(n):
     fig.append_trace({
         'x': water_depth_filtered[:,0],
         'y': water_depth_filtered[:,1],
-        'name': 'water depth filtered',
+        'name': 'water volume filtered',
         'mode': 'lines',
         'type': 'scatter',
         'line': {'dash': 'solid', 'color': 'blue'},
@@ -192,7 +183,7 @@ def update_plots_live(n):
     fig.update_yaxes(title_text='pH', row=1, col=1)
     fig.update_yaxes(title_text='temperature (' + deg_sign + 'C)', row=3, col=1)
     fig.update_yaxes(title_text='Humidity (%)', row=4, col=1)
-    fig.update_yaxes(title_text='depth (inches)',row=5,col=1)
+    fig.update_yaxes(title_text='volume (L)',row=5,col=1)
     fig.update_xaxes(title_text='time (hours)', row=1, col=1)
     fig.update_xaxes(title_text='time (hours)', row=2, col=1)
     fig.update_xaxes(title_text='time (hours)', row=3, col=1)
