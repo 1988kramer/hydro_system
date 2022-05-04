1384a5b8c318d06341eb3d309abcd8ad56723e0f
Andrew Kramer <1988kramer@gmail.com>
2022-05-03 19:32:37 -0600

    deactivated logging

--- nodes/MPL3115A2_node.py
+++ nodes/MPL3115A2_node.py
@@ -162,10 +162,10 @@ class MPL3115A2_Node:
     pressure_msg.header.stamp = rospy.Time.now()
     pressure_msg.value = water_volume_in_liters
 
-    with open('/home/pi/logs/pressure_kPa.txt', 'a') as f:
-      f.write('%.4f,%.4f,%.4f\n' % (pressure_msg.header.stamp.to_sec(), 
-                                    sensor_pressure_kPa, 
-                                    barometric_pressure_kPa))
+    #with open('/home/pi/logs/pressure_kPa.txt', 'a') as f:
+    #  f.write('%.4f,%.4f,%.4f\n' % (pressure_msg.header.stamp.to_sec(), 
+    #                                sensor_pressure_kPa, 
+    #                                barometric_pressure_kPa))
 
     self.depth_pub.publish(pressure_msg)
 
